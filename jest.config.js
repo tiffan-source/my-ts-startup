50ee3632bc41ad9dfdb2ec9b1bd093f6a8a57e39
tiffan-source <zohoungbogboanlyou@gmail.com>
2025-06-10 07:22:41 +0200

    chore: setup jest

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,15 @@
-import { createDefaultEsmPreset } from 'ts-jest'
+import { createDefaultEsmPreset } from "ts-jest";
 
 const tsJestTransformCfg = createDefaultEsmPreset().transform;
 
 /** @type {import("jest").Config} **/
 export default {
-  testEnvironment: "node",
-  transform: {
-    ...tsJestTransformCfg,
-  },
-  collectCoverage: true,
+   testEnvironment: "node",
+   transform: {
+      ...tsJestTransformCfg,
+   },
+   collectCoverage: true,
    coverageDirectory: "coverage",
-   collectCoverageFrom: [
-    "src/**/*.ts",
-   ],
-   rootDir: "./tests"
+   collectCoverageFrom: ["src/**/*.ts"],
+   rootDir: "./tests",
 };
